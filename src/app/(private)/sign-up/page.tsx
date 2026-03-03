7fd062f2e32757c63b24a646fd21a1bfee3137a7
Guilherme Bueno dos Reis <guilhermebuenoreis.contact@gmail.com>
2026-03-03 19:19:00 -0300

    inital commit

---
+++ src/app/(private)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <main>
+      <div>Signup</div>
+    </main>
+  );
+}
---
+++ src/app/(public)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashbaord() {
+  return (
+    <main>
+      <div>Dashbaord</div>
+    </main>
+  );
+}
